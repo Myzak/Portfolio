8c21cdd27a1d4b5b1717f2724aac0f97def29c76
Myzak <muzakki09876@gmail.com>
2023-10-14 23:20:41 +0700

    update

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,10 +2,15 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
     header.classList.remove("navbar-fixed");
   }
 };
@@ -19,3 +24,5 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+//to-top
